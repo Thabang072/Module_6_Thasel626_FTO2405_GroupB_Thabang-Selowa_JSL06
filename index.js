66ca847c7117fd5de65ca6c1726e46dd17f97186
Thabang Selowa <mohale20cobra@gmail.com>
2024-07-27 15:09:46 +0200

    Add Loop through category and its item menu

--- index.js
+++ index.js
@@ -6,10 +6,39 @@ const menu = {
 
 };
 
+const price = {
+    GarlicBread: "R1",
+    Bruschetta: "R30",
+    MargheritaPiz: "R300"
+}
+
+
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
-    const displayMenuItems = document.getElementById("menu");
+   
+    const OurMenu = document.getElementById("menu");
+    
+        forEach(OurMenu => OurMenu { MainCourses =  MainCourses ...}
+            
+        });
+
+            // Take menu items array display as ordered list before ordering
+
+            // create unordered list <ul>
+
+            // populate unordered onclick() list with each item from the the menu array.
+
+            // return menu
+
+            // console.log(category + ":");
+            // menu[category].forEach(item => console.log(" - " + item));
+            // console.log(); // Adding a blank line for better readability
+        }
+    
+    
+    displayMenu(menu);
+    
     
    
     // Loop through each category and its items in the menu object
@@ -26,19 +55,22 @@ function displayMenuItems(menu) {
 
         // Loop through the items in the category and create list items
 
-            // Create a list item element
+        // Create a list item element
 
-            // Set the text content of the list item element to the item name
+        // Set the text content of the list item element to the item name
 
-            // Attach a click event listener to the list item to add it to the order
+        // Attach a click event listener to the list item to add it to the order
 
-            // Append the list item to the list of items
+        // Append the list item to the list of items
     }
             
 }
 
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
+
+    const OurMenu = document.getElementById("order-items");
+    // items clicked => add in ul () [iterare in the menu using food const and add it to the ul, keep incrementing food++ as long as onclick() ]
     // Get the order items list and the order total element from the HTML
 
     // Create a list item for the order
@@ -55,6 +87,7 @@ function addToOrder(itemName) {
 // Function to initialize the menu system
 function initMenuSystem(menu) {
     // Call the function to display menu items
+    displayMenuItems();
 }
 
 // Start the menu system by calling the init function
